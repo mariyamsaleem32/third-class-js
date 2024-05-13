b6707888cdee1c905d68cbb8d6c54af27fb88d8a
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-05-13 15:50:51 +0500

    Add files via upload

---
+++ javascript start/script-s.js
@@ -0,0 +1,51 @@
+let a = 3;
+let b = --a;
+console.log(a);
+console.log(b);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+console.log(newNum);
+
+// ++a   //pre increment
+// --aa  //pre decriment
+// a++   //post increment
+// a--   //post decriment
+
+// let number1 = parseInt(prompt("enter your first number",33));
+// let number2 = parseInt(prompt("enter your second number",27));
+// console.log(number1 + number2);
+
+// let x = 21;
+// x += 4;
+// console.log(x);
+
+// let m = 8;
+// let r = 8;
+// console.log(m===r);
+
+// let mood = "dark";
+// let color = "black";
+// if (mood == "dark"){
+//     console.log(color);
+// }
+// else{
+//     console.log(color);
+// }
+
+// let user = prompt("enter your name");
+// let age = +prompt("enter your age");
+// if (age >= 18){
+//         console.log("you are aligible for vote");
+//     }
+//     else  {
+//             console.log("you are not aligible for vote" );
+//         }
+
+let number = +prompt ("enter your number");
+if (number %2 ==0){
+    console.log(number, "is even number");
+}
+else{
+    console.log(number,"is odd number");
+}
